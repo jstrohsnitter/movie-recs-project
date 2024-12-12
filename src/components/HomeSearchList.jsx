1509a58a22db943a3176fbdfabb247b95c47e0a8
jstrohsnitter <jstrohsnitter@gmail.com>
2024-12-12 16:06:57 -0500

    add button adds searched film to database

--- src/components/HomeSearchList.jsx
+++ src/components/HomeSearchList.jsx
@@ -1,10 +1,58 @@
+import MovieCard from "./MovieCard/MovieCard"
+import axios from 'axios'
+
+
+
+
 const HomeSearchList = (props) => {
 
-    const foundTitle = props.foundTitle
+    const movie = props.foundTitle
+
+
+    const handleAdd = () => {
+        //event.preventDefault()
+            let data = JSON.stringify({
+            "title": `${movie.Title}`,
+            "plot": `${movie.Plot}`,
+            "runtime": `${movie.Runtime}`,
+            "rated": `${movie.Rated}`,
+            "released": `${movie.Released}`,
+            "genre": `${movie.Genre}`,
+            "poster": `${movie.Poster}`,
+            "watched": "false",
+            "director": `${movie.Director}`,
+            "actors": `${movie.Actors}`,
+            "awards": `${movie.Awards}`,
+            });
+
+            let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'http://3.90.140.106:3001/movies',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            data : data
+            };
+
+            axios.request(config)
+            .then((response) => {
+            console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+            console.log(error);
+            });
+    }
+
 
     return (
         <>
-        <p>Title: {foundTitle.Title}</p>
+        {movie === '' ? <br></br> :
+        <>
+        <MovieCard movie={movie}/>
+        <button onClick={handleAdd}>Add To Watchlist</button>
+        </>
+    }
         </>
     )
 }
