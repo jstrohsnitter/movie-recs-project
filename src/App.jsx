dac5d03ff4363603ce3ae420d24c9566cad84bfb
milestarricone <milestarricone2@gmail.com>
2024-12-13 10:10:58 -0500

    added navbar, edited watchlist/moviecard to intake correct data,

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-import { useState } from 'react'
-import './App.css'
-import HomeSearch from '/src/components/HomeSearch.jsx'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import HomeSearch from '/src/components/HomeSearch.jsx';
+import Watchlist from './components/Watchlist/Watchlist';
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
+  const [NavBarState, setNavBarState] = useState('Home');
+  const [movies, setMovies] = useState([]);
+
+  const handleNavBar = (theChosenPage) => {
+    setNavBarState(theChosenPage);
+  };
+
+  const renderPage = () => {
+    switch (NavBarState) {
+      case 'Home':
+        return <HomeSearch />;
+      case 'Watchlist':
+        return <Watchlist movies={movies} />;
+      default:
+        return <HomeSearch />;
+    }
+  };
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch('http://3.90.140.106:3001/movies');
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
 
   return (
     <>
-    <HomeSearch />
-    {/* <h1>Hello world!</h1>
-    <h2>Hello again</h2> */}
+      <NavBar handleNavBar={handleNavBar} />
+      {renderPage()}
     </>
   );
-}
+};
 
-export default App
+export default App;
